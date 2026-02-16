8f2181ab025cc2ea9c6cc26df7f5e466067045dd
JahongirOfficial <jrprogrammer10@gmail.com>
2026-02-16 16:50:13 +0500

    Add debug logging

--- server/index.ts
+++ server/index.ts
@@ -52,6 +52,13 @@ export function createServer() {
 
   // Middleware
   app.use(cors());
+  
+  // Debug middleware to log all requests
+  app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`);
+    next();
+  });
+  
   app.use(express.json({ limit: '50mb' }));
   app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
